e4e0191ea7011e10f69d798fe8b157ebb1b3e5ce
NeoHamzah <amirulhamzah758@gmail.com>
2023-02-01 01:34:14 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab eveniet culpa temporibus voluptates perspiciatis nam itaque quidem tempora commodi perferendis? Asperiores doloremque numquam porro maiores accusamus illum sint eos quia.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
